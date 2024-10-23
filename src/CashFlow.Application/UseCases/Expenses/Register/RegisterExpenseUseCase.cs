bd869c27a6a44949b25a72ae89062e75b78a7b4a
Douglas Rufino <douglasrufinosilva@gmail.com>
2024-10-22 22:02:01 -0300

    validação dos dados de registro

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -17,11 +18,31 @@ public class RegisterExpenseUseCase
 
     private void Validate(RequestRegisterExpenseJson request)
     {
-        var title = request.Title;
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
 
-        if (string.IsNullOrWhiteSpace(title))
+        if (titleIsEmpty)
         {
-
+            throw new ArgumentException("O título é obrigatório.");
         }
+
+        if(request.Amount <= 0)
+        {
+            throw new ArgumentException("O Valor deve ser maior que zero.");
+        }
+
+        var dateResult = DateTime.Compare(request.Date, DateTime.UtcNow);
+
+        if(dateResult > 0)
+        {
+            throw new ArgumentException("Insira uma data válida.");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+
+        if (!paymentTypeIsValid)
+        {
+            throw new ArgumentException("Tipo de pagamento inválido.");
+        }
+
     }
 }
