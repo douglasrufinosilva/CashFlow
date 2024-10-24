8366206c2b90a27f6bf9dee921a937f962cfd2d5
Douglas Rufino <douglasrufinosilva@gmail.com>
2024-10-24 16:14:14 -0300

    tratamento de erros com o try catch

--- src/CashFlow.Api/Controllers/ExpenseController.cs
+++ src/CashFlow.Api/Controllers/ExpenseController.cs
@@ -12,9 +12,20 @@ public class ExpenseController : ControllerBase
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
     {
-        var useCase = new RegisterExpenseUseCase();
-        var response = useCase.Execute(request);
+        try
+        {
+            var useCase = new RegisterExpenseUseCase();
+            var response = useCase.Execute(request);
 
-        return Created(string.Empty, response);
+            return Created(string.Empty, response);
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "unknow error");
+        }
     }
 }
